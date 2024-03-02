ab36cdb5b9d0d55380c35a216dbfd5d6c8398498
geeth <sulakshanageeth@gmail.com>
2024-03-03 01:31:38 +0530

    Finalized the nav bar

--- src/Screens/dashboard/dashboard.js
+++ src/Screens/dashboard/dashboard.js
@@ -16,38 +16,51 @@ function App() {
 
   return (
     <div>
-      <Row>
-        <Col xs lg="2" className="bg-light "></Col>
-        <Col className="bg-light">
-          <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
-            <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      Action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                      Separated link
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </Col>
-      </Row>
+      <div
+        class=" bg-body-tertiary text-center"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          zIndex: 100,
+          padding: 0,
+        }}
+      >
+        <div class="row">
+          <div class="bg-body-tertiary col-2"></div>
+          <div class="col" style={{ padding: 0 }}>
+            <Navbar expand="lg" className="bg-body-tertiary">
+              <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">
+                        Action
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">
+                        Another action
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action/3.4">
+                        Separated link
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          </div>
+        </div>
+      </div>
+
       {/* Placeholder for the Navbar to maintain the layout */}
       <div style={{ height: navHeight }}></div>
       <Row>
