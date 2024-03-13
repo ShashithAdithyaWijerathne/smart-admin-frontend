3cc1917bf2f0a182975202b01a67a22fd6a6b769
geeth <sulakshanageeth@gmail.com>
2024-03-13 21:53:39 +0530

    Create separate side bar using vanilla css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import Login from "./Screens/login/login";
-import Dashboard from "./Screens/dashboard/dashboard";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./Screens/login/login";
+// import Dashboard from "./Screens/dashboard/dashboard";
+import SideNavBar from "./SideNavBar/SideNavBar";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/fdProject" element={<Dashboard />} />
+//         <Route path="/users" element={<Dashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/fdProject" element={<Dashboard />} />
-        <Route path="/users" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <SideNavBar />
+    </div>
   );
 }
 
--- src/Screens/dashboard/dashboard.js
+++ src/Screens/dashboard/dashboard.js
@@ -5,6 +5,7 @@ import Calender from "./calender";
 import FdProduct from "../fdProduct/fdProduct";
 import Users from "../Users/users";
 import "./dashboard.css";
+import SidebarMenu from "react-bootstrap-sidebar-menu";
 import { FaBook } from "react-icons/fa";
 
 function App() {
